9e79b5118da427f3f70902e2004ecb5f0c06c4f5
Christophe Michel <christopheml@users.noreply.github.com>
2020-05-01 01:42:06 +0200

    Indexed the board by line first.

--- src/main/java/com/github/christopheml/fastblocks/core/Board.java
+++ src/main/java/com/github/christopheml/fastblocks/core/Board.java
@@ -13,18 +13,17 @@ public class Board {
     public static final int COLUMNS = 12;
     public static final int LINES = 22;
 
-    private final Block[][] board = new Block[COLUMNS][LINES];
-
+    private final Block[][] board = new Block[LINES][COLUMNS];
 
     public void lock(Piece piece) {
-        piece.blocksPositions().forEach(p -> board[p.x][p.y] = new DeadBlock(p, piece.shape().color));
+        piece.blocksPositions().forEach(p -> board[p.y][p.x] = new DeadBlock(p, piece.shape().color));
     }
 
     public boolean isOccupied(Point p) {
         if (collidesLeftSide(p) || collidesRightSide(p) || p.y < 0 || p.y >= LINES) {
             return false;
         }
-        return board[p.x][p.y] != null;
+        return board[p.y][p.x] != null;
     }
 
     boolean collidesVerticalTop(Point point) {
